632be7bd2d97c2c3b2792095ce21eaba1cbf90da
Youngyz1 <uche2net@gmail.com>
2025-12-06 18:17:21 +0000

    gitaction-runner

--- backend.tf
+++ backend.tf
@@ -1,7 +1,9 @@
 terraform {
   backend "s3" {
-    bucket = "ofiliyoungyzcloud1"
-    key    = "ofiliyoungyz/production/terraform.tfstate"
-    region = "us-east-1"
+    bucket         = "ofiliyoungyzcloud1"
+    key            = "ofiliyoungyz/production/terraform.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "terraform-locks"
+    encrypt        = true
   }
 }
