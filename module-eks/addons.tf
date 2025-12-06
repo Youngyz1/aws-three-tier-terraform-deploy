5c9550c231699d82c515d0509e181c63ff156e2f
Youngyz1 <uche2net@gmail.com>
2025-12-06 20:14:31 +0000

    updated cicd

--- module-eks/addons.tf
+++ module-eks/addons.tf
@@ -1,13 +1,16 @@
+# Get EKS authentication token
 data "aws_eks_cluster_auth" "eks" {
   name = aws_eks_cluster.eks.name
 }
 
+# Kubernetes provider
 provider "kubernetes" {
   host                   = aws_eks_cluster.eks.endpoint
   cluster_ca_certificate = base64decode(aws_eks_cluster.eks.certificate_authority[0].data)
   token                  = data.aws_eks_cluster_auth.eks.token
 }
 
+# Helm provider
 provider "helm" {
   kubernetes = {
     host                   = aws_eks_cluster.eks.endpoint
@@ -16,6 +19,7 @@ provider "helm" {
   }
 }
 
+# Install NGINX Ingress Controller
 resource "helm_release" "nginx_ingress" {
   name             = "nginx-ingress"
   repository       = "https://kubernetes.github.io/ingress-nginx"
@@ -28,14 +32,22 @@ resource "helm_release" "nginx_ingress" {
   depends_on = [aws_eks_node_group.eks_node_group]
 }
 
-data "aws_lb" "nginx_ingress" {
-  tags = {
-    "kubernetes.io/service-name" = "ingress-nginx/nginx-ingress-ingress-nginx-controller"
+# Get the Load Balancer from the nginx-ingress Service
+data "kubernetes_service" "nginx_ingress" {
+  metadata {
+    name      = "nginx-ingress-ingress-nginx-controller"
+    namespace = "ingress-nginx"
   }
 
   depends_on = [helm_release.nginx_ingress]
 }
 
+# Output the LB hostname
+output "nginx_ingress_lb_hostname" {
+  value = data.kubernetes_service.nginx_ingress.status[0].load_balancer[0].ingress[0].hostname
+}
+
+# Install cert-manager
 resource "helm_release" "cert_manager" {
   name             = "cert-manager"
   repository       = "https://charts.jetstack.io"
@@ -52,6 +64,7 @@ resource "helm_release" "cert_manager" {
   depends_on = [helm_release.nginx_ingress]
 }
 
+# Install ArgoCD
 resource "helm_release" "argocd" {
   name             = "argocd"
   repository       = "https://argoproj.github.io/argo-helm"
